40d406f24bb70321e10e15c2bdcdc2bd2d4d28f9
heeheemcgee <67600753+michaelhu22@users.noreply.github.com>
2023-02-26 23:26:27 -0500

    bugfix for PlayerHandler.cs index calculation

--- BlockThePet/Assets/Scripts/PlayerHandler.cs
+++ BlockThePet/Assets/Scripts/PlayerHandler.cs
@@ -9,17 +9,15 @@ namespace GameHandler{
     {
         // fence prefab is set to "Top Layer" layer tag
         [SerializeField] GameObject fencePrefab;
-        static double boardSize = 10.0;
-        static double boardMultipler = boardSize/10.0;
-        static double xOffset = 5.0;
-        static double yOffset = 0.0;
-
-        
-        // fencePrefab.transform.localScale = new Vector3(3.2*boardMultipler, 3.2*boardMultipler, 1);
+        static double boardSize = 10;
+        static double boardMultipler = boardSize/10;
+        static double xOffset = 5;
+        static double yOffset = 0;
 
         // Start is called before the first frame update
         void Start()
         {
+            Debug.Log(boardMultipler);
         }
 
         // Update is called once per frame
@@ -30,19 +28,19 @@ namespace GameHandler{
                 double newX = System.Math.Floor((spawnPosition.x - 5)*boardMultipler);
                 double newY = System.Math.Floor((spawnPosition.y) *boardMultipler);
 
+                // getting array indices 
+                int row = (int)(5*boardMultipler - newY - 1);
+                int col = (int)(newX + 10*boardMultipler);
+                Debug.Log("row: " + row);
+                Debug.Log("col: " + col);
+
+                // get new x and y coords based on fence index later
                 newX = ((newX+0.5)/boardMultipler) + xOffset;
                 newY = ((newY+0.5)/boardMultipler) + yOffset;
 
                 Vector2 centeredSpawn = new Vector2((float)newX, (float)newY);
-                // GameObject g = Instantiate(fencePrefab, (Vector2)spawnPosition, Quaternion.identity);
                 GameObject fence = Instantiate(fencePrefab, centeredSpawn, Quaternion.identity);
                 fence.transform.localScale = new Vector3((float)(3.2/boardMultipler), (float)(3.2/boardMultipler), 1);
-
-                // getting array indices
-                int row = (int)(4*boardMultipler - newY);
-                int col = (int)(newX + 10*boardMultipler);
-                Debug.Log("row: " + row);
-                Debug.Log("col: " + col);
             }
         }
 
