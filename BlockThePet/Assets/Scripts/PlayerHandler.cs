b86dfd3109c859bcb6165fd97291e801ec082c12
heeheemcgee <67600753+michaelhu22@users.noreply.github.com>
2023-02-26 23:06:45 -0500

    made fence placements in PlayerHandler.cs/
    fence placement transform works with any board density, and gives array index to correspond to Tile array.  testing placeFence() functionality in update() until GameHandler.cs is fixed

--- BlockThePet/Assets/Scripts/PlayerHandler.cs
+++ BlockThePet/Assets/Scripts/PlayerHandler.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using MathF;
 
 
 namespace GameHandler{
     public class PlayerHandler : MonoBehaviour
     {
+        // fence prefab is set to "Top Layer" layer tag
         [SerializeField] GameObject fencePrefab;
+        static double boardSize = 10.0;
+        static double boardMultipler = boardSize/10.0;
+        static double xOffset = 5.0;
+        static double yOffset = 0.0;
+
+        
+        // fencePrefab.transform.localScale = new Vector3(3.2*boardMultipler, 3.2*boardMultipler, 1);
+
         // Start is called before the first frame update
         void Start()
         {
@@ -16,13 +26,34 @@ namespace GameHandler{
         void Update()
         {
             if(Input.GetMouseButtonDown(0)){
-                Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                GameObject g = Instantiate(fencePrefab, (Vector2)spawnPosition, Quaternion.identity);
+                Vector2 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                double newX = System.Math.Floor((spawnPosition.x - 5)*boardMultipler);
+                double newY = System.Math.Floor((spawnPosition.y) *boardMultipler);
+
+                newX = ((newX+0.5)/boardMultipler) + xOffset;
+                newY = ((newY+0.5)/boardMultipler) + yOffset;
+
+                Vector2 centeredSpawn = new Vector2((float)newX, (float)newY);
+                // GameObject g = Instantiate(fencePrefab, (Vector2)spawnPosition, Quaternion.identity);
+                GameObject fence = Instantiate(fencePrefab, centeredSpawn, Quaternion.identity);
+                fence.transform.localScale = new Vector3((float)(3.2/boardMultipler), (float)(3.2/boardMultipler), 1);
+
+                // getting array indices
+                int row = (int)(4*boardMultipler - newY);
+                int col = (int)(newX + 10*boardMultipler);
+                Debug.Log("row: " + row);
+                Debug.Log("col: " + col);
             }
         }
 
+        public Tile[,] placeFence(Tile[,] board) {
+            // TO DO
+            // once can edit gamehandler, make it so fences can only be placed on empty spaces
+            return board;
+        }
+
         public void test() {
-            Debug.Log("bum bum");
+            Debug.Log("test");
         }
     }
 }
