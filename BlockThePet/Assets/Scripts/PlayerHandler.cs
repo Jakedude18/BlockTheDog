1c838facf5c1d1e08c00c90b4e99fa4c5d7579dd
Jake Kerrigan <36115570+Jakedude18@users.noreply.github.com>
2023-02-27 14:37:41 -0500

    block the pig

--- BlockThePet/Assets/Scripts/PlayerHandler.cs
+++ BlockThePet/Assets/Scripts/PlayerHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 // using MathF;
 
 
@@ -23,30 +24,28 @@ namespace GameHandler{
         // Update is called once per frame
         void Update()
         {
-            if(Input.GetMouseButtonDown(0)){
-                Vector2 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                double newX = System.Math.Floor((spawnPosition.x - 5)*boardMultipler);
-                double newY = System.Math.Floor((spawnPosition.y) *boardMultipler);
-
-                // getting array indices 
-                int row = (int)(5*boardMultipler - newY - 1);
-                int col = (int)(newX + 10*boardMultipler);
-                Debug.Log("row: " + row);
-                Debug.Log("col: " + col);
-
-                // get new x and y coords based on fence index later
-                newX = ((newX+0.5)/boardMultipler) + xOffset;
-                newY = ((newY+0.5)/boardMultipler) + yOffset;
-
-                Vector2 centeredSpawn = new Vector2((float)newX, (float)newY);
-                GameObject fence = Instantiate(fencePrefab, centeredSpawn, Quaternion.identity);
-                fence.transform.localScale = new Vector3((float)(3.2/boardMultipler), (float)(3.2/boardMultipler), 1);
-            }
+            
         }
 
         public Tile[,] placeFence(Tile[,] board) {
-            // TO DO
-            // once can edit gamehandler, make it so fences can only be placed on empty spaces
+            Vector2 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            double newX = System.Math.Floor((spawnPosition.x - 5)*boardMultipler);
+            double newY = System.Math.Floor((spawnPosition.y) *boardMultipler);
+
+            // getting array indices 
+            int row = (int)(5*boardMultipler - newY - 1);
+            int col = (int)(newX + 10*boardMultipler);
+            Debug.Log("row: " + row);
+            Debug.Log("col: " + col);
+
+
+
+            if(board[row,col].GetType() != typeof(Grass)){
+                throw new System.Exception("Hello");
+            }
+            else{
+                board[row,col] = new Fence();
+            }
             return board;
         }
 
